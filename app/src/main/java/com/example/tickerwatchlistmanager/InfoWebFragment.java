4726fae24ce03cc77f317a89c12f108aff8e059b
annabui9 <annabui1009@gmail.com>
2025-09-27 09:27:48 -0500

    Fixed xml. now displays both fragments and has seekingarray.com being displayed.

--- app/src/main/java/com/example/tickerwatchlistmanager/InfoWebFragment.java
+++ app/src/main/java/com/example/tickerwatchlistmanager/InfoWebFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,34 +16,15 @@ import android.view.ViewGroup;
  */
 public class InfoWebFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    WebView infoWebWidget;
 
     public InfoWebFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InfoWebFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static InfoWebFragment newInstance(String param1, String param2) {
         InfoWebFragment fragment = new InfoWebFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -50,8 +32,7 @@ public class InfoWebFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +40,11 @@ public class InfoWebFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_web, container, false);
+        View view = inflater.inflate(R.layout.fragment_info_web, container, false);
+        infoWebWidget = view.findViewById(R.id.infoWeb);
+        infoWebWidget.getSettings().setJavaScriptEnabled(true);
+        infoWebWidget.loadUrl("https://seekingalpha.com");
+
+        return view;
     }
 }
--- app/src/main/java/com/example/tickerwatchlistmanager/MainActivity.java
+++ app/src/main/java/com/example/tickerwatchlistmanager/MainActivity.java
@@ -1,25 +1,45 @@
 package com.example.tickerwatchlistmanager;
 
+import android.net.wifi.ScanResult;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 // 2 fragments, one is a listview, the other is a webview
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        if (savedInstanceState == null){
+            fg = getSupportFragmentManager();
+            FragmentTransaction trans = fg.beginTransaction();
+            TickerListFragment tlf = new TickerListFragment();
+            trans.add(R.id.TickerSymbolsFCV, tlf, "tickerListFrag");
+            InfoWebFragment iwf = new InfoWebFragment();
+            trans.add(R.id.InfoWebFCV, iwf, "infoWebFrag");
+            trans.commit();
+            Log.d("","anna" + findViewById(R.id.InfoWebFCV).toString());
+
+
+        }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
